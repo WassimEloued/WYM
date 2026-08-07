52a9f6b93603e52819750468216511912edd9373
Wassim Eloued <elouedwassim@gmail.com>
2026-08-07 18:58:24 +0100

    WYM Backend
    Backend service for WYM

---
+++ BACKEND/src/test/java/wym/rentcar/RentcarApplicationTests.java
@@ -0,0 +1,13 @@
+package wym.rentcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentcarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
